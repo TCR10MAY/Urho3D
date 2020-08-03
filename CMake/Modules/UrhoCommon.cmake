86a1c7f2a5b6d2cf3011dfef39955ce88a494adf
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2020-08-03 23:14:10 +0800

    Refactor to avoid hard-coding the library arch for Debian distro. Instead, assume the GCC (cross-)compiler toolchain will have the "--enable-multiarch" enabled and the toolchain has a triplet matching the CMAKE_LIBRARY_ARCHITECTURE_REGEX which is set to "[a-z0-9_]+(-[a-z0-9_]+)?-linux-gnu[a-z0-9_]*" as of this writing. If so, CMake will automatically initialize the CMAKE_LIBRARY_ARCHITECTURE with a right value and CMake will use it when searching for libraries, not requiring the build script to spell out the extra path suffixes manually.

--- CMake/Modules/UrhoCommon.cmake
+++ CMake/Modules/UrhoCommon.cmake
@@ -123,17 +123,14 @@ include (CheckHost)
 include (CheckCompilerToolchain)
 
 # Extra linker flags for linking against indirect dependencies (linking shared lib with dependencies)
-if (RPI)
-    if (NOT NATIVE_64BIT)
-        # Extra linker flags for Raspbian because it installs VideoCore libraries in the "/opt/vc/lib" directory (no harm in doing so for other distros)
-        set (INDIRECT_DEPS_EXE_LINKER_FLAGS "${INDIRECT_DEPS_EXE_LINKER_FLAGS} -Wl,-rpath-link,\"${CMAKE_SYSROOT}/opt/vc/lib\"")      # CMAKE_SYSROOT is empty when not cross-compiling
-    else ()
-        set (INDIRECT_DEPS_EXE_LINKER_FLAGS "${INDIRECT_DEPS_EXE_LINKER_FLAGS} -Wl,-rpath-link,\"${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu\"")
-    endif ()
+if (RPI AND NOT RPI_ABI STREQUAL RPI4)
+    # Extra linker flags for legacy Raspbian because it installs VideoCore libraries in the "/opt/vc/lib" directory (no harm in doing so for other distros)
+    set (INDIRECT_DEPS_EXE_LINKER_FLAGS "${INDIRECT_DEPS_EXE_LINKER_FLAGS} -Wl,-rpath-link,\"${CMAKE_SYSROOT}/opt/vc/lib\"")      # CMAKE_SYSROOT is empty when not cross-compiling
 elseif (APPLE AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 8.0.0)
     set (INDIRECT_DEPS_EXE_LINKER_FLAGS "${INDIRECT_DEPS_EXE_LINKER_FLAGS} -Wl,-no_weak_imports")
 endif ()
 if (ARM AND CMAKE_SYSTEM_NAME STREQUAL Linux AND CMAKE_CROSSCOMPILING)
+    message ("!!! I am here ${CMAKE_LIBRARY_ARCHITECTURE}")
     # Cannot do this in the toolchain file because CMAKE_LIBRARY_ARCHITECTURE is not yet defined when CMake is processing toolchain file
     set (INDIRECT_DEPS_EXE_LINKER_FLAGS "${INDIRECT_DEPS_EXE_LINKER_FLAGS} -Wl,-rpath-link,\"${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}\":\"${CMAKE_SYSROOT}/lib/${CMAKE_LIBRARY_ARCHITECTURE}\"")
 endif ()
